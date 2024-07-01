981020edba82d65658773d747a084227dc699724
sanjaygupta972004 <sanjaygupta07054@gmail.com>
2024-07-01 22:01:47 +0530

    text subscription controller and correct all issue

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -2,8 +2,8 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { Subscription } from "../models/subscription.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { User } from "../models/user.model.js"; 
-import { isValidObjectId } from "mongoose";
+import { User } from "../models/user.model.js";
+import mongoose, { isValidObjectId } from "mongoose";
 
 
 const toggleToSubscription = asyncHandler(async(req,res)=> {
@@ -12,11 +12,11 @@ const toggleToSubscription = asyncHandler(async(req,res)=> {
   if(!isValidObjectId(channelId)){
         throw new ApiError(400,"Invalid channel id")
   }
-   const channel = await User.findById(channel)
+   const channel = await User.findById(channelId)
         if(!channel){
           throw new ApiError(404,"Channel does not exist in the database")      
         }
-   const subscription = await subscription.findOne({
+   const subscription = await Subscription.findOne({
         $and:[
                 {subscriber:userId},
                 {channel:channelId}
@@ -48,43 +48,64 @@ const getUserChannelSubscribers = asyncHandler(async(req,res)=> {
         if(!isValidObjectId(channelId)){
                 throw new ApiError(400,"Invalid channel id")
         }
-     
+
         const subscribers =  await Subscription.aggregate([
                 {
-                        $match:{channel:channelId}
-                },
-                {
-                        $lookup:{
-                                from:"users",
-                                localField:"subscriber",
-                                foreignField:"_id",
-                                as:"subscriber"
+                        $match:{
+                                channel:new mongoose.Types.ObjectId(channelId)
                         }
                 },
                 {
-                        $addFields:{
-                             totalSubscriber:{
-                                $size:"$subscriber"
-                             }
-                        }
-                },
-                {
-                        $project:{
-                                _id:1,
-                                subscriber:1,
-                                totalSubscriber:1
-                        }
-                }
+                        $facet:{
+                                subscribers:[
+                                        {
+                                                $lookup:{
+                                                        from:"users",
+                                                        localField:"subscriber",
+                                                        foreignField:"_id",
+                                                        as:"subscriber"
+                                                }
+                                        },
+                                        {
+                                                $unwind:"$subscriber"
+                                        },
+                                        {
+                                                $project:{
+                                                        _id:1,
+                                                        subscriber:{
+                                                                username:1,
+                                                                email:1,
+                                                                avatar:1,
+                                                                watchHistory:1,
+                                                                _id:1  ,
+                                                                createdAt:1,
+                                                                updatedAt:1
+                                                        },
+                                                        
+                                                }
+                                        }
+                                ],
+                                totalSubscribers:[
+                                        {
+                                                $count:"subscribers"
+                                        },
+                                        {
+                                                $unwind:"$subscribers"
+                                        }
+                                ]
+                        },
+                },      
       
-      ])
+   ])
 
-if(subscribers.length===0){
+  
+  if(subscribers.length===0){
         throw new ApiError(404,"No subscribers found for this channel")
-}
+  }
 
-return res
+  return res
         .status(200)
-        .json(new ApiResponse(200,subscribers,"Subscribers found for this channel"))
+        .json(new ApiResponse(200,subscribers[0],"Subscribers found for this channel"))
         
 })
 
@@ -94,7 +115,9 @@ const getSubscribedChannels = asyncHandler(async(req,res)=> {
   const subscriberId = req.user?._id;
         const subscribedChannel = await Subscription.aggregate([
                 {
-                        $match:{subscriber:subscriberId}
+                        $match:{
+                                subscriber:new mongoose.Types.ObjectId(subscriberId)
+                        }
                 },
                 {
                         $lookup:{
@@ -105,23 +128,31 @@ const getSubscribedChannels = asyncHandler(async(req,res)=> {
                         }
                 },
                 {
-                        $addFields:{
-                                totalChannel:{
-                                        $size:"$channel"
-                                }
-                        }
+                        $unwind:"$channel"
                 },
                 {
                         $project:{
                                 _id:1,
-                                channel:1,
-                                totalChannel:1
+                                channel:{
+                                        username:1,
+                                        email:1,
+                                        avatar:1,
+                                        watchHistory:1,
+                                        _id:1  ,
+                                        createdAt:1,
+                                        updatedAt:1
+                                
+                                },
                         }
-                }
+                },
+              
         ])
+
         if(subscribedChannel.length===0){
                 throw new ApiError(404,"No channel found")
         }
+        const totalSubscribedChannels = subscribedChannel.length;
+        subscribedChannel.push({totalSubscribedChannels})
         return res
                 .status(200)
                 .json(new ApiResponse(200,subscribedChannel,"Subscribed channels found for this user"))
--- src/routers/subscription.router.js
+++ src/routers/subscription.router.js
@@ -1,6 +1,16 @@
 import {Router} from 'express';
 
 const router = Router();
+import { jwtVerify} from '../middlewares/auth.middleware.js';
+import { 
+        toggleToSubscription,
+        getUserChannelSubscribers,
+        getSubscribedChannels
+ } from '../controllers/subscription.controller.js';
 
+ router.use(jwtVerify);
+ router.route("/toggleToSubscription/c/:channelId").post(toggleToSubscription);   
+ router.route("/getUserChannelSubscribers/c/:channelId").get(getUserChannelSubscribers)
+ router.route("/getSubscribedChannels").get(getSubscribedChannels);
 
 export default router;
